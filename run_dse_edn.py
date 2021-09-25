365e22c7425aba4cd57737c93db1e0bbc7c9a9ba
DhananjayaWijerathne <dmdwijerathne@gmail.com>
2021-09-25 22:32:58 +0800

    Adding design space explore script for edn

---
+++ run_dse_edn.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#export TESTBENCH_HOME=/home/dmd/Workplace/HiMap
+import numpy as np
+import sys
+import os
+import os.path
+import math
+import matplotlib
+import run_morpher_edn
+import time
+import pandas as pd
+import datetime
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+
+himap2_config = pd.read_csv("config_himap.csv", index_col ="Application")
+
+print(himap2_config)
+
+HIMAP2_HOME = '/home/dmd/Workplace/HiMap2'	
+
+if True:
+  # ## Run design space exploration
+  print("Design space exploration")
+  entry_id = 0
+  for i, j in himap2_config.iterrows():
+      os.chdir(HIMAP2_HOME+'/HiMap2_Scripts')
+      print('python run_morpher_edn.py  %s %s %s %s %s %s %s %s %s %s %s &' % (j["N"],j["init"], j["C1"],j["C2"],j["r"],j["c"],j["arch_desc"],j["maxiter"],j["skip"],j["oslimit"],entry_id))
+      if True:
+        os.system('python run_morpher_edn.py  %s %s %s %s %s %s %s %s %s %s %s &' % (j["N"],j["init"], j["C1"],j["C2"],j["r"],j["c"],j["arch_desc"],j["maxiter"],j["skip"],j["oslimit"],entry_id))
+      entry_id = entry_id+1
+
+
--- run_morpher_edn.py
+++ run_morpher_edn.py
@@ -3,6 +3,7 @@ import sys
 import os
 import os.path
 import shutil
+import time 
 ############################################
 # Directory Structure:
 # Morpher Home:
@@ -13,7 +14,7 @@ import shutil
 
 # Build all three tools before running this script
 
-def main():
+def main(no_clusters, no_init,C1_init_, C2_init_, cgra_cluster_r, cgra_cluster_c, arch_desc, maxIter,skip_inter_or_intra, open_set_limit, entry_id):
 
   HIMAP2_HOME = '/home/dmd/Workplace/HiMap2'	
   #if not 'HIMAP2_HOME' in os.environ:
@@ -25,15 +26,32 @@ def main():
   MAPPER_HOME = HIMAP2_HOME + '/Morpher_CGRA_Mapper'
   #SIMULATOR_HOME = HIMAP2_HOME + '/hycube_simulator'
 
+  dir_name = 'Entry_%s_Clusters_%s_Init_%s_C1_%s_C2_%s_r_%s_c_%s_arch_%s_maxIter_%s_skip_%s_oslimit_%s/' % (entry_id,no_clusters, no_init,C1_init_, C2_init_, cgra_cluster_r, cgra_cluster_c, arch_desc, maxIter,skip_inter_or_intra, open_set_limit)
+  sum_log_name = '_entry_%s_clusters_%s_Init_%s_C1_%s_C2_%s_r_%s_c_%s_arch_%s_maxIter_%s_skip_%s_oslimit_%s' % (entry_id,no_clusters, no_init,C1_init_, C2_init_, cgra_cluster_r, cgra_cluster_c, arch_desc, maxIter,skip_inter_or_intra, open_set_limit)
+
   DFG_GEN_KERNEL = DFG_GEN_HOME + '/applications/edn/'
-  DFG_CLUSTRNG_KERNEL = DFG_CLUSTRNG_HOME + '/applications/edn/'
-  MAPPER_KERNEL = MAPPER_HOME + '/applications/clustered_arch/edn/'
-  #SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/array_add/'
+  DFG_CLUSTRNG_KERNEL = DFG_CLUSTRNG_HOME + '/applications/edn/' + dir_name
+  MAPPER_KERNEL = MAPPER_HOME + '/applications/clustered_arch/edn/'+ dir_name
+  EXECTIME_SUMMARY = HIMAP2_HOME + '/HiMap2_Scripts/exec_time/edn/' 
+  #SIMULATOR_KERNEL =SIMULATOR_HOME + '/applications/'
 
   my_mkdir(DFG_GEN_KERNEL)
   my_mkdir(DFG_CLUSTRNG_KERNEL)
   my_mkdir(MAPPER_KERNEL)
-  #my_mkdir(SIMULATOR_KERNEL)
+  my_mkdir(EXECTIME_SUMMARY)
+
+  #dir_path = os.path.join(KERNEL_HOME, dir_name)
+  #print('Changing directory to %s' % dfg_path)
+  #if not os.path.isdir(dfg_path):
+  #	os.makedirs(dfg_path)
+  sum_file_name = EXECTIME_SUMMARY+'summary'+sum_log_name+'.log'
+   
+     
+  
+  f=open(sum_file_name, "a+")
+  f.write("\n**********************************\n")
+  f.write("Clustering: \n Number of Clusters: %s\n Init: %s\n C1: %s\n C2: %s\n CGRA r: %s\n CGRA c: %s \n\nCGRA Mapping: \n Architecture: %s\n MaxIter: %s\n Skip Intra or inter %s\n Open set limit: %s" % (no_clusters, no_init,C1_init_, C2_init_, cgra_cluster_r, cgra_cluster_c, arch_desc, maxIter,skip_inter_or_intra, open_set_limit))
+
 
 
 
@@ -49,18 +67,15 @@ def main():
 
   print('\nRunning DFG Clustering\n')
   os.chdir(DFG_CLUSTRNG_KERNEL)
-  #os.system('python ../../dfg_clustering.py jpegdct_POST_LN111_PartPred_DFG_forclustering.xml 7 %s 100' % ('precomputed'))
-  os.system('dot -Tpdf inter_cluster.dot -o inter_cluster_graph_edn_7_100.pdf')
-  os.system('cp clustered.png clustered_7_100.png')
+  os.system('python ../../../dfg_clustering.py jpegdct_POST_LN111_PartPred_DFG_forclustering.xml %s %s %s > log1.txt' % (no_clusters, ('precomputed'), no_init))
+  os.system('dot -Tpdf inter_cluster.dot -o inter_cluster_graph_edn_%s_%s.pdf' % (no_clusters, no_init))
+  os.system('cp clustered.png clustered_%s_%s.png' % (no_clusters, no_init))
 	
-  #os.system('python ../../dfg_clustering.py jpegdct_POST_LN111_PartPred_DFG_forclustering.xml 10 %s 100' % ('precomputed'))
-  #os.system('dot -Tpdf inter_cluster.dot -o inter_cluster_graph_edn_10_100.pdf')
-  #os.system('cp clustered.png clustered_10_100.png')
   os.system('cp clustering_outcome.txt '+ MAPPER_KERNEL)
   os.system('cp inter_cluster_edges.txt '+ MAPPER_KERNEL)
   
   print('\nRunning modified SPKM cluster mapping\n')
-  #os.system('python ../../cluster_level_mapping_spkmModified.py 2 2 3 3')
+  os.system('python ../../../cluster_level_mapping_spkmModified.py %s %s %s %s > log2.txt' % (C1_init_,C2_init_,cgra_cluster_r,cgra_cluster_c))
   os.system('cp dfg_to_cgra_cluster_mapping_outcome.txt '+ MAPPER_KERNEL)
 
 
@@ -70,12 +85,16 @@ def main():
 ##############################################################################################################################################
   print('\nRunning Morpher_CGRA_Mapper\n')
   os.chdir(MAPPER_KERNEL)
+  start = time.time()
 
-  os.system(HIMAP2_HOME+'/Morpher_CGRA_Mapper/build/src/cgra_xml_mapper -m 60 -d jpegdct_POST_LN111_PartPred_DFG.xml -j '+HIMAP2_HOME+'/Morpher_CGRA_Mapper/json_arch/clustered_archs/stdnoc_3x3tiles_3x3PEs.json')
-  os.system('neato -Tpdf %s -o %s' % ('arch_allconnections.dot','stdnoc_3x3tiles_4x4PEs.pdf'))
-  os.system('neato -Tpdf %s -o %s' % ('arch_interclusterconnections.dot','stdnoc_3x3tiles_4x4PEs_interclusterconnections.pdf'))
+  os.system(HIMAP2_HOME+'/Morpher_CGRA_Mapper/build/src/cgra_xml_mapper -m %s -d jpegdct_POST_LN111_PartPred_DFG.xml -j %s -s %s -l %s > log.txt &' % (maxIter,HIMAP2_HOME+'/Morpher_CGRA_Mapper/json_arch/clustered_archs/'+arch_desc, skip_inter_or_intra, open_set_limit))
+  os.system('neato -Tpdf arch_allconnections.dot -o %s.pdf' % (arch_desc))
+  os.system('neato -Tpdf arch_interclusterconnections.dot -o %s_interclusterconnections.pdf' % (arch_desc))
 
-  #os.system('../../../build/src/cgra_xml_mapper -d array_add_INNERMOST_LN1_PartPred_DFG.xml -x 4 -y 4 -j hycube_original_mem.json -t HyCUBE_4REG')
+  end = time.time() 
+
+  f.write("\n\nExecution time: %s"% (end - start))   
+  f.close() 
 
 
 def my_mkdir(dir):
@@ -85,4 +104,15 @@ def my_mkdir(dir):
         pass
 
 if __name__ == '__main__':
-  main()
+    no_clusters = sys.argv[1]
+    no_init = sys.argv[2]    
+    C1_init = sys.argv[3]
+    C2_init = sys.argv[4]
+    cgra_cluster_r = sys.argv[5]
+    cgra_cluster_c = sys.argv[6]
+    arch_desc = sys.argv[7]
+    maxIter = sys.argv[8]
+    skip_inter_or_intra = sys.argv[9]    
+    open_set_limit = sys.argv[10]  
+    entry_id = sys.argv[11]
+    main(no_clusters, no_init,C1_init, C2_init,cgra_cluster_r, cgra_cluster_c,arch_desc, maxIter,skip_inter_or_intra, open_set_limit, entry_id)
